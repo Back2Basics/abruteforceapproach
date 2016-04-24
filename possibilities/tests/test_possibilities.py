93d74c86c5122c4d303010aa5205e0db097e3fb7
Jeremy Langley <gettingBack2Basics@gmail.com>
2016-04-23 21:25:12 -0500

    change to unittests

--- possibilities/tests/test_possibilities.py
+++ possibilities/tests/test_possibilities.py
@@ -1,22 +1,33 @@
 import numpy as np
-import nose
-import unittest
+from nose import with_setup
 from possibilities.posibility_generator import possibilities
-
-class test_absolute_value(unittest.TestCase):
-    def test_abs():
-        start = np.array([-5])
-        finish = np.array([5])
-        assert(possibilities(start = start, finish=finish, libs=[np])
-
-class test_transpose(unittest.TestCase):
+import unittest
+def setup_transpose():
+    start = np.arange(6).reshape(2, 3)
+    finish = np.array([[0, 3],
+                                    [1, 4],
+                                    [2, 5]]
+    )
     
-    def test_transpose():
-        start = np.arange(6).reshape(2, 3)
-        finish = np.array([[0, 3],
-                                        [1, 4],
-                                        [2, 5]]
-        )
-        self.assert_in('transpose',possibilities(start = start, finish=finish, libs=[np]))
 
 
+class test_unary_possibilities(unittest.TestCase):
+    def setUp(self):
+        self.start = np.array([-5])
+        self.finish = np.array([5])
+    
+    def tearDown(self):
+        del(self.start)
+        del(self.finish)
+    
+
+    def test_abs(self):
+        self.assertTrue(possibilities(start = self.start, finish=self.finish, libs=[np]))
+    
+    def test_transpose(self):
+        self.assertIn('transpose' , possibilities(start = self.start, finish=self.finish, libs=[np]))
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+    
